b6d350f8041d3af132a4e65025c85c74f81744b1
daze <leon.baiocchi@studio.unibo.it>
2020-11-13 21:17:42 +0000

    add javadoc

--- src/it/unibo/oop/lab/enum2/Place.java
+++ src/it/unibo/oop/lab/enum2/Place.java
@@ -1,5 +1,12 @@
 package it.unibo.oop.lab.enum2;
 
+/** 
+ * 
+ * Defines the constants INDOOR and OUTDOOR
+ * they represent the places in which the 
+ * {@link Sport} is practiced
+ *
+ */
 public enum Place {
 	INDOOR, OUTDOOR;
 }
